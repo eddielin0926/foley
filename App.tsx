dd8abd19890c6a603fa061c71f0af0c3bdc35893
Eddie Lin <eddielin0926@gmail.com>
2021-12-14 03:04:44 +0800

    feat: add react native navigation & elements

--- App.tsx
+++ App.tsx
@@ -1,21 +1,36 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import HomeScreen from "~/screens/HomeScreen";
+import ProcessScreen from "~/screens/ProcessScreen";
+
+export type StackParams = {
+  HomeScreen: undefined;
+  ProcessScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<StackParams>();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{ title: "首頁" }}
+          />
+          <Stack.Screen
+            name="ProcessScreen"
+            component={ProcessScreen}
+            options={{ title: "流程" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
