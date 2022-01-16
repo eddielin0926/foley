9cf01d9b99348a076de2583055045a5e4c54ecc4
Hans Liu <hansliu40815@gmail.com>
2022-01-16 16:00:28 +0800

    feat: :sparkles: add axios.ts and db.json

---
+++ src/axios.ts
@@ -0,0 +1,22 @@
+import _axios from "axios"
+
+const axios = () => {
+    //建立一個自定義的axios
+    const instance = _axios.create({
+            baseURL: 'http://localhost:3000',
+            timeout: 1000,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    
+    instance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );    
+     return instance;
+}
+
+export {axios};
